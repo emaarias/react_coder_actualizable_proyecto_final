ad5d862ad6bc0bcf6683dcba2aa81ca4a59f472d
emaarias <emanuel.arias@groovinads.com>
2022-03-05 23:47:48 -0300

    Clase 3 - Tienda Nav

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 function Menu() {
   return (
 	<header className="header">
-  <a href="#" className="logo">Nav Carrito EA</a>
+  <a href="#" className="logo">INDUMENTARIA EA</a>
   <input className="menu-btn" type="checkbox" id="menu-btn" />
   <label className="menu-icon" for="menu-btn"><span className="navicon"></span></label>
   <ul className="menu">
-    <li><a href="#">Home</a></li>
-    <li><a href="#">Nosotros</a></li>
-    <li><a href="#">Productos</a></li>
-    <li><a href="#">Contacto</a></li>
+    <li><a href="#">HOMBRE</a></li>
+    <li><a href="#">MUJER</a></li>
+    <li><a href="#">NIÑOS</a></li>
+    <li><a href="#">SALE</a></li>
   </ul>
 </header>
   )
