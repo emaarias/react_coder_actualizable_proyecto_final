c3a295b78037a71abc9649af59579f97abee9ddf
emaarias <emanuel.arias@groovinads.com>
2022-03-31 09:17:35 -0300

    .

--- src/App.js
+++ src/App.js
@@ -5,31 +5,39 @@ import Menu from './components/menu/menu';
 import ItemListContainer from './container/itemListContainer';
 import ItemDetailContainer from './components/itemDetailContainer/itemDetailContainer';
 import Cart from './components/cart/cart';
+import { CartContext } from './context/cartContext';
 
 /* import ItemCount from './container/itemCount'; */
 
-
+console.log('cart context ',CartContext);
 
 function App() {
   return (
 
     <BrowserRouter>
 
-      <div className="App">
-        <Menu />
-
-        <main className="App-header">
-    <Routes>
-          <Route path='/' element ={<ItemListContainer greeting="👋 Hola próximamente seré un catálogo de productos!" />}/>
-          <Route path='/gender/:idGender' element ={<ItemListContainer greeting="👋 Hola próximamente seré un catálogo de productos!" />}/>
-          <Route path='detalle/:idDetail' element ={<ItemDetailContainer />}/>
-          <Route path='/*' element ={<Navigate to='/' />}/>
-          <Route path='cart' element ={<Cart />}/>
+      <CartContext.Provider>
 
 
-    </Routes>
-        </main>
-      </div>
+        <div className="App">
+          <Menu />
+
+          <main className="App-header">
+            <Routes>
+              <Route path='/' element={<ItemListContainer greeting="👋 Hola próximamente seré un catálogo de productos!" />} />
+              <Route path='/gender/:idGender' element={<ItemListContainer greeting="👋 Hola próximamente seré un catálogo de productos!" />} />
+              <Route path='detalle/:idDetail' element={<ItemDetailContainer />} />
+              <Route path='/*' element={<Navigate to='/' />} />
+              <Route path='cart' element={<Cart />} />
+
+
+            </Routes>
+          </main>
+        </div>
+
+      </CartContext.Provider>
+
+
     </BrowserRouter>
 
   );
